ac797d8e4a019740ab229d30301b84d447c89b43
lachlan <lachlan@gardnerfamily.dynalias.com>
2019-10-19 12:02:50 +1100

    polished

--- globe.py
+++ globe.py
@@ -1,9 +1,12 @@
 import discord
-from discord.ext import commands
 
 # global variables
 
+# server IDs
 fserv_id = 624784883251675136
+
+# channels
+react_id = 626340503918739456
 welcome_id = 624785035437801492
 intro_id = 627986269900898325
 audit_id = 625115043356475394
@@ -14,6 +17,7 @@ admin = 625116240771284994
 mod = 625116299227299840
 muted = 625248374127591434
 
+# roles
 member_role = 633963699413188609
 rule_id = 624784883251675138
 cmd_id = 625115068862038016
@@ -22,22 +26,20 @@ qotd_id = 626172681955573775
 bot_2_id = 627985907013910538
 bday_id = 631704872516845579
 
-react_id = 626340503918739456
-
+# emojis
 errorx = "<:redcross:608943524075012117>"
 upvote = "<:upvote:625819552466468895>"
 downvote = "<:downvote:625819552177192966>"
+tick = "<:greentick:608943523823222785>"
 
 
 pending_events = []
 
 
-def check_mod(ctx):
-    if isinstance(ctx, discord.Member):
-        author = ctx
-    else:
-        author = ctx.author
-    role_ids = [x.id for x in author.roles]
+# checks
+
+def check_mod(member):
+    role_ids = [x.id for x in member.roles]
     if set(role_ids) & {mod, admin}:
         return True
     else:
@@ -46,24 +48,3 @@ def check_mod(ctx):
 
 def check_main_serv(ctx):
     return fserv_id == ctx.guild.id
-
-# def check_heads(ctx):
-#     role_ids = [x.id for x in ctx.author.roles]
-#     if set(role_ids) & {admin, head}:
-#         return True
-#     else:
-#         return False
-#
-#
-# def check_no_t(ctx):
-#     if isinstance(ctx, commands.Context):
-#         author = ctx.author
-#     elif isinstance(ctx, discord.Member):
-#         author = ctx
-#     else:
-#         author = ctx.author
-#     role_ids = [x.id for x in author.roles]
-#     if set(role_ids) & {mod, admin, head}:
-#         return True
-#     else:
-#         return False
