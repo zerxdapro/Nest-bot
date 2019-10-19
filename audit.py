67ff8ceb91c4113567a88a121929c071de4a9096
lachlan <lachlan@gardnerfamily.dynalias.com>
2019-10-20 09:40:31 +1100

    polish

--- audit.py
+++ audit.py
@@ -8,8 +8,9 @@ class Logging(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener()
+    @commands.check(globe.check_main_serv)
     async def on_message_delete(self, message):
-        if not message.guild.id == globe.fserv_id:
+        if not message.guild.id == globe.serv_id:
             return
         if message.author.bot:
             return
@@ -17,14 +18,13 @@ class Logging(commands.Cog):
         embed.set_author(name=message.author.display_name, icon_url=message.author.avatar_url)
         embed.set_footer(text=message.created_at.strftime("%-I:%M%p, %-d %b %Y"))
 
-        server = self.bot.get_guild(globe.fserv_id)
+        server = self.bot.get_guild(globe.serv_id)
         channel = server.get_channel(globe.audit_id)
         await channel.send(embed=embed)
 
     @commands.Cog.listener()
+    @commands.check(globe.check_main_serv)
     async def on_message_edit(self, old_message, message):
-        if not message.guild.id == globe.fserv_id:
-            return
         if message.author.bot:
             return
         if message.content == old_message.content:
@@ -36,7 +36,7 @@ class Logging(commands.Cog):
         if message.edited_at:
             embed.set_footer(text=message.edited_at.strftime("%-I:%M%p, %-d %b %Y"))
 
-        server = self.bot.get_guild(globe.fserv_id)
+        server = self.bot.get_guild(globe.serv_id)
         channel = server.get_channel(globe.audit_id)
         await channel.send(embed=embed)
     
@@ -50,16 +50,15 @@ class Logging(commands.Cog):
     #     await channel.send(embed=embed)
     
     @commands.Cog.listener()
+    @commands.check(globe.check_main_serv)
     async def on_member_update(self, old, new):
-        if not new.guild.id == globe.fserv_id:
-            return
         if old.display_name != new.display_name:
             embed = discord.Embed(colour=0xe4e458, description=f"**Username changed**")
             embed.set_author(name=new.display_name, icon_url=new.avatar_url)
             embed.add_field(name="Old", value=old.display_name, inline=False)
             embed.add_field(name="New", value=new.display_name, inline=False)
 
-            server = self.bot.get_guild(globe.fserv_id)
+            server = self.bot.get_guild(globe.serv_id)
             channel = server.get_channel(globe.audit_id)
             await channel.send(embed=embed)
         elif old.roles != new.roles:
@@ -75,7 +74,7 @@ class Logging(commands.Cog):
             embed.add_field(name="Role:", value=role, inline=False)
             embed.set_author(name=new.display_name, icon_url=new.avatar_url)
 
-            server = self.bot.get_guild(globe.fserv_id)
+            server = self.bot.get_guild(globe.serv_id)
             channel = server.get_channel(globe.audit_id)
             await channel.send(embed=embed)
 
