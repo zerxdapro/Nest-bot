50da1668e61bd9554b2fccc966a1132b4c628296
lachlan <lachlan@gardnerfamily.dynalias.com>
2019-10-19 15:44:13 +1100

    Added some comments and changed multiline comments to be correct

--- main.py
+++ main.py
@@ -13,58 +13,70 @@ cogs = ["basic", "moderation", "rules", "audit", "member", "pin", "invites", "pu
 
 @bot.event
 async def on_ready():
-    print(bot.user.name + " has started")
-    print("-"*len(bot.user.name + " is running"))
+    startup = bot.user.name + " is running"
+    print(startup)
+    print("-"*len(startup))
+
+    # Set activity for member count
     count = len([x for x in bot.get_guild(globe.fserv_id).members if not x.bot])
     status = f"{count} members!"
     await bot.change_presence(status=discord.Status('online'), activity=discord.Activity(type=discord.ActivityType.watching, name=status))
+
+    # Import all the commands
     bot.remove_command('help')
     for i in cogs:
         print("Loading extension: " + i)
         bot.load_extension("" + i)
 
+    # Grab the invites for logging
     invites = bot.get_cog("Invites")
     if not invites:
         print("-------error loading invites---------")
     else:
         await invites.setup()
-    # gsh.append_row([dt.now().strftime("%d/%m/%Y %H:%M:%S"), count])
+
+    """
+    gsh.append_row([dt.now().strftime("%d/%m/%Y %H:%M:%S"), count])
 
     # resume timers
 
-    # with open("timers.csv", "r") as file:
-    #     reader = csv.reader(file, delimiter=",")
-    #     reader = list(reader)
-    #     for i in reader[1:]:
-    #         server = bot.get_guild(fserv_id)
-    #         channel = server.get_channel(int(i[2]))
-    #         try:
-    #             message = await channel.fetch_message(int(i[1]))
-    #             author = server.get_member(int(i[0]))
-    #             inp_time = dt.strptime(i[3], '%Y-%m-%d %H:%M:%S.%f')
-    #             time = (inp_time - dt.now()).total_seconds()
-    #             if time <= 0:
-    #                 reader.remove(i)
-    #                 embed = message.embeds[0]
-    #                 new_embed = discord.Embed(title=embed.title, colour=embed.colour, description="Timer is up!")
-    #                 await message.edit(embed=new_embed)
-    #             else:
-    #                 bot.loop.create_task(timer_loop(time, message, author))
-    #         except discord.NotFound:
-    #             pass
+    with open("timers.csv", "r") as file:
+        reader = csv.reader(file, delimiter=",")
+        reader = list(reader)
+        for i in reader[1:]:
+            server = bot.get_guild(fserv_id)
+            channel = server.get_channel(int(i[2]))
+            try:
+                message = await channel.fetch_message(int(i[1]))
+                author = server.get_member(int(i[0]))
+                inp_time = dt.strptime(i[3], '%Y-%m-%d %H:%M:%S.%f')
+                time = (inp_time - dt.now()).total_seconds()
+                if time <= 0:
+                    reader.remove(i)
+                    embed = message.embeds[0]
+                    new_embed = discord.Embed(title=embed.title, colour=embed.colour, description="Timer is up!")
+                    await message.edit(embed=new_embed)
+                else:
+                    bot.loop.create_task(timer_loop(time, message, author))
+            except discord.NotFound:
+                pass
 
-    # header = ['Author', 'Message', 'Channel', 'Time']
-    # with open('timers.csv', 'wt') as f:
-    #     csv_writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-    #     csv_writer.writerow(header)  # write header
-    #     csv_writer.writerows(reader[1:])
+    header = ['Author', 'Message', 'Channel', 'Time']
+    with open('timers.csv', 'wt') as f:
+        csv_writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        csv_writer.writerow(header)  # write header
+        csv_writer.writerows(reader[1:])
 
     # end timers
+    """
 
 
 @bot.command()
 @commands.is_owner()
 async def kidnap(ctx, user: discord.Member):
+    """
+    Don't ask
+    """
     server = bot.get_guild(globe.fserv_id)
     muted = server.get_role(globe.muted)
     channel = 625608405528215552
@@ -78,6 +90,9 @@ async def kidnap(ctx, user: discord.Member):
 @bot.command()
 @commands.is_owner()
 async def edit(ctx, message, *, content):
+    """
+    Edit a message from the bot
+    """
     msg = await ctx.channel.fetch_message(int(message))
     await msg.edit(content=content)
 
