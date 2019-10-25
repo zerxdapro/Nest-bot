8c48bc059ed9b90f5488d49ce09cd70cf07634e6
lachlan <lachlan@gardnerfamily.dynalias.com>
2019-10-26 07:54:59 +1100

    Improved the story stuff

--- cogs/story.py
+++ cogs/story.py
@@ -22,7 +22,7 @@ class Story(commands.Cog):
 
         if not story_channel(ctx):
             return
-        elif ctx.author == self.bot.user:
+        elif ctx.author.id == self.bot.user.id:
             return
         elif ctx.author == self.user_buffer:  # if author is the last person to add
             await ctx.channel.send(f"{globe.errorx} You were the last person to add to the story", delete_after=5)
@@ -36,7 +36,7 @@ class Story(commands.Cog):
             await ctx.channel.send(f"{globe.errorx} Your message can't contain a '@' in it", delete_after=5)
             await ctx.delete(delay=5)
             return
-        elif len(msg) > 35:
+        elif len(msg) > 20:
             await ctx.channel.send(f"{globe.errorx} Your addition is too long", delete_after=5)
             await ctx.delete(delay=5)
             return
@@ -47,11 +47,17 @@ class Story(commands.Cog):
             content = content.split("\n", 2)[2]
         else:
             content = ""
-        appended = "(The last person to add to the story was {})\n\n{} {}".format(ctx.author.display_name, content, msg)
+
+        if msg == ".":
+            appended = f"(The last person to add to the story was {ctx.author.display_name})\n\n{content}. "
+        else:
+            appended = f"(The last person to add to the story was {ctx.author.display_name})\n\n{content} {msg}"
 
         if len(appended) > 2000:
+            await story_msg.edit(content=story_msg.content.split("\n", 2)[2])
             appended = "(The last person to add to the story was {})\n\n{}".format(ctx.author.display_name, msg)
             story_msg = await ctx.channel.send(appended)
+
             self.story_id = story_msg.id
             self.bot_data["story_msg"] = story_msg.id
             yaml.dump(self.bot_data, open("data/bot.yml", "w"))
